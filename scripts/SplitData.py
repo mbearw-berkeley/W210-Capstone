9a7d47a5d3c7a42e80305f937b2eff5dbe923813
mbearw <60677866+mbearw-berkeley@users.noreply.github.com>
2021-07-28 22:41:10 -0700

    File cleanup and split data

--- scripts/SplitData.py
+++ scripts/SplitData.py
@@ -1,8 +1,11 @@
 import pandas as pd,numpy as np
 from sklearn.model_selection import train_test_split
 def main():
-	data = pd.read_csv("./data/CleanedData/fullData.csv")
+	data = pd.read_csv("./data/CleanData/fullData.csv")
 
+
+	predict_set = data[data["YEAR BEING FORECAST"]>=2022]
+	predict_set.to_csv('./data/PredictionData/predict_set.csv',index=False)
 	relevant = data[data["YEAR FORECAST MADE"]>=1992][['YEAR BEING FORECAST', 'INDICATOR', 'actual', 'MAX', 'ACTUAL_CONF',
        'HIT', 'pred_average', 'pred_var', 'banana', 'beef', 'bread', 'chicken',
        'coffee', 'eggs', 'electricity', 'flour', 'iceCream', 'rice',
@@ -14,4 +17,6 @@ def main():
 	train.to_csv("./data/TrainingData/train.csv",index=False)
 	test.to_csv("./data/TestData/test.csv",index=False)
 	val.to_csv("./data/ValidationData/val.csv",index=False)
+
+
 main()
