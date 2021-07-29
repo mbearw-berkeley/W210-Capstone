5b8b0ec46de44562786e82fadb96d1f6a5df6167
mbearw <60677866+mbearw-berkeley@users.noreply.github.com>
2021-07-29 15:24:40 -0700

    Checked unemployment data dates

--- scripts/SplitData.py
+++ scripts/SplitData.py
@@ -2,14 +2,14 @@ import pandas as pd,numpy as np
 from sklearn.model_selection import train_test_split
 def main():
 	data = pd.read_csv("./data/CleanData/fullData.csv")
-
-
 	predict_set = data[data["YEAR BEING FORECAST"]>=2022]
 	predict_set.to_csv('./data/PredictionData/predict_set.csv',index=False)
 	relevant = data[data["YEAR FORECAST MADE"]>=1992][['YEAR BEING FORECAST', 'INDICATOR', 'actual', 'MAX', 'ACTUAL_CONF',
        'HIT', 'pred_average', 'pred_var', 'banana', 'beef', 'bread', 'chicken',
-       'coffee', 'eggs', 'electricity', 'flour', 'iceCream', 'rice',
+       'eggs', 'electricity', 'flour', 'iceCream',
        'unleadedGasoline']]
+    predict_set = relevant[relevant["YEAR BEING FORECAST"]>=2022]
+	predict_set.to_csv('./data/PredictionData/predict_set.csv',index=False)
 	relevant.dropna(inplace=True)
 
 	train,test = train_test_split(relevant,test_size=.2,random_state=42,stratify=relevant["INDICATOR"])
--- update.py
+++ update.py
@@ -2,18 +2,18 @@ import rpy2.robjects as robjects,sys
 
 sys.path.insert(0,"scripts")
 
-import UpdateSPF
-import CPI_cleaning
-import PCE_cleaning
-import GDP_cleaning
-import Unemp_cleaning
+# import UpdateSPF
+# import CPI_cleaning
+# import PCE_cleaning
+# import GDP_cleaning
+# import Unemp_cleaning
 
 
-r = robjects.r
-r['source']('scripts/preprocess.R')
+# r = robjects.r
+# r['source']('scripts/preprocess.R')
 
-import AddPrices
+# import AddPrices
 
-print("Data update completed! ")
+# print("Data update completed! ")
 import SplitData
 print("Data split completed!")
